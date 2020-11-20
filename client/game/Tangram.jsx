fa271c97557b94a3b83c166f603ff46ac8f3bbe0
Robert Hawkins <rxdh@stanford.edu>
2020-11-20 15:01:30 -0800

    display as background of div

--- client/game/Tangram.jsx
+++ client/game/Tangram.jsx
@@ -11,13 +11,20 @@ export default class Tangram extends React.Component {
   render() {
     const { tangram, isDeck, stage, ...rest } = this.props;
     const { clicked } = this.state;
+    const mystyle = {
+      "background" : "url(" + tangram + ")",
+      "background-size": "cover",
+      "width" : "25%",
+      'height': cw + 'px'
+      
+    };
     const target = stage.get("target");
     return (
       <div
         onClick={this.handleClick}
         className={`bp3-card`}
+        style={mystyle}
         >
-        <img src={tangram}></img>
       </div>
     );
   }
