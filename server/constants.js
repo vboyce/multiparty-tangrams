cb4b8b4d200e87599859f04999aa59c819334d2d
irinaliu <72238483+irinaliu@users.noreply.github.com>
2021-01-11 11:56:09 -0500

    Uploading new jdenticons and avatar name colors

--- server/constants.js
+++ server/constants.js
@@ -32,28 +32,57 @@ export const names = [
     "Magenta",
   ]; // for the players names to match avatar color
 
+// Blue avatar names and color codes:
 export const avatarNames = [
-    "Colton",
-    "Aaron",
-    "Alex",
-    "Tristan",
-    "Daniel",
-    "Jill",
-    "Jimmy",
-    "Adam",
-    "Flynn",
-    "Annalise",
+    "Lincoln",
+    "Leo",
+    "Kayla",
+    "Molly",
+    "Oliver",
+    "Lucy",
+    "Aria",
+    "Elijah",
+    "Evan",
+    "Katherine",
    ]; // to do more go to https://jdenticon.com/#icon-D3
 
 export const nameColor = [
-    "#3D50B7",
-    "#70A945",
-    "#DE8AAB",
-    "#A59144",
-    "#DER5F4",
-    "#EB8TWV",
-    "#N0WFA4",
-    "#TP3BWU",
-    "#QW7MI9",
-    "#EB8TWj",
+    "#7A9CDC",
+    "#5697C3",
+    "#6DBCD2",
+    "#2D7496",
+    "#448BC4",
+    "#5AA4C3",
+    "#2A8393",
+    "#4EABC5",
+    "#4173B5",
+    "#4649AB",
   ]; // similar to the color of the avatar
+
+// Red avatar names and color codes:
+// export const avatarNames = [
+//     "Claire",
+//     "Jill",
+//     "Asher",
+//     "Wyatt",
+//     "Jake",
+//     "Cole",
+//     "Declan",
+//     "Jack",
+//     "Sarah",
+//     "Kaylee",
+// ]; // to do more go to https://jdenticon.com/#icon-D3
+
+// export const nameColor = [
+//     "#A33C49",
+//     "#B55D42",
+//     "#BB786C",
+//     "#BB6C7C",
+//     "#CE7771",
+//     "#A33F56",
+//     "#C56D67",
+//     "#C56777",
+//     "#C56767",
+//     "#A04744",
+// ]; // similar to the color of the avatar
+
