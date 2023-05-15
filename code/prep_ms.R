e94e5d6651226fd0098c1e1e3a2411082e217340
Veronica Boyce <vboyce@mit.edu>
2023-05-15 16:55:15 -0700

    start emoji analysis

--- code/prep_ms.R
+++ code/prep_ms.R
@@ -31,7 +31,7 @@ data_location_2c <- "data/study2c"
 two_c_rounds_include <- read_rds(here(data_location_2c,"rounds_include.rds"))
 two_c_round_results <- read_rds(here(data_location_2c,"round_results.rds")) |> mutate(rotate="emoji")
 two_c_chat <- read_csv(here(data_location_2c, "chat.csv")) |> mutate(rotate="emoji")
-
+two_c_raw <- read_csv(here(data_location_2c, "raw_chat.csv")) |> inner_join(two_c_rounds_include) |> filter(role=="listener") |> mutate(condition="emoji", activePlayerCount=6)
 
 ### study 3
 
@@ -39,7 +39,8 @@ data_location_3 <- "data/study3"
 three_rounds_include <- read_rds(here(data_location_3,"rounds_include.rds"))
 three_round_results <- read_rds(here(data_location_3,"round_results.rds")) |> rename(`_id`="X_id", condition=name)
 three_chat <- read_csv(here(data_location_3, "chat.csv")) |> rename(`_id`="X_id",condition=name)
-
+three_raw <- read_csv(here(data_location_3, "raw_chat.csv")) |> inner_join(three_rounds_include) |>   rename(condition=name) |> 
+  filter(condition %in% c("2_thin", "6_thin")) |> filter(role=="listener")
 ### Combined
 
 combined_results <- one_round_results |> 
@@ -57,3 +58,5 @@ combined_chat <- one_chat |>
   mutate(activePlayerCount=NA) |> 
   rename(condition=rotate) |> 
   union(three_chat)
+
+combined_emoji <- two_c_raw |> union(three_raw)
